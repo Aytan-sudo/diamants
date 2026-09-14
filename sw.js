f86d4ef3558c2092a4c0df761767be26339130f4
Aytan-sudo <cantais.aymeric@gmail.com>
2026-09-14 16:40:57 +0200

    fix: conserver les caches des autres jeux

--- sw.js
+++ sw.js
@@ -39,7 +39,7 @@ self.addEventListener('install', (evenement) => {
 self.addEventListener('activate', (evenement) => {
   evenement.waitUntil(
     caches.keys()
-      .then((cles) => Promise.all(cles.filter((cle) => cle !== VERSION).map((cle) => caches.delete(cle))))
+      .then((cles) => Promise.all(cles.filter((cle) => cle.startsWith('diamants-') && cle !== VERSION).map((cle) => caches.delete(cle))))
       .then(() => self.clients.claim()),
   );
 });
